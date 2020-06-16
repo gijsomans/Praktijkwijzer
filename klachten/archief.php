c03f60e6b54b5a06bde21142fac82eaf5889876e
Chokio <jrnchn@gmail.com>
2020-06-16 09:11:33 +0200

    nog een fix

--- klachten/archief.php
+++ klachten/archief.php
@@ -30,34 +30,10 @@ include '../includes/db.php';
                         <li>
                             <a href="archief.php">Klachten archief</a>
                         </li>
-                        <li>
-                            <a href="#">Home 3</a>
-                        </li>
+                      
                     </ul>
                 </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
-                    <ul class="collapse list-unstyled" id="pageSubmenu">
-                        <li>
-                            <a href="#">Page 1</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 2</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 3</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">Portfolio</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
+               
             </ul>
         </nav>
         <?php
